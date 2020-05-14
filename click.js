87c48559a94b77fd005e30dfb4a4af424d67d2bc
puneethdc99 <58505120+puneethdc99@users.noreply.github.com>
2020-05-14 22:39:44 +0530

    Update click.js

--- click.js
+++ click.js
@@ -15,5 +15,60 @@ function LoadThirdImage() {
 
 function saveImage() {
   var imagedata = document.getElementById("fullImage").src;
-  document.getElementById("printbtn").setAttribute("href", imagedata);
+  if (imagedata.includes("shapes.svg")) {
+    let input = imagedata;
+    new SvgToPngConverter().convertFromInput(input, function (imgD) {
+      document.getElementById("printbtn").setAttribute("href", imgD);
+    });
+  } else {
+    document.getElementById("printbtn").setAttribute("href", imagedata);
+  }
+}
+
+function LoadImage4() {
+  document.getElementById("fullImage").src = "./images/shapes.svg";
+  bloomingMenu.remove();
+}
+
+class SvgToPngConverter {
+  constructor() {
+    this._init = this._init.bind(this);
+    this._cleanUp = this._cleanUp.bind(this);
+    this.convertFromInput = this.convertFromInput.bind(this);
+  }
+
+  _init() {
+    this.canvas = document.createElement("canvas");
+    this.imgPreview = document.createElement("img");
+    this.imgPreview.style = "position: absolute; top: -9999px";
+
+    document.body.appendChild(this.imgPreview);
+    this.canvasCtx = this.canvas.getContext("2d");
+  }
+
+  _cleanUp() {
+    document.body.removeChild(this.imgPreview);
+  }
+
+  convertFromInput(input, callback) {
+    this._init();
+    let _this = this;
+    this.imgPreview.onload = function () {
+      const img = new Image();
+      _this.canvas.width = _this.imgPreview.clientWidth;
+      _this.canvas.height = _this.imgPreview.clientHeight;
+      img.crossOrigin = "anonymous";
+      img.src = _this.imgPreview.src;
+      img.onload = function () {
+        _this.canvasCtx.drawImage(img, 0, 0);
+        let imgData = _this.canvas.toDataURL("image/png");
+        if (typeof callback == "function") {
+          callback(imgData);
+        }
+        _this._cleanUp();
+      };
+    };
+
+    this.imgPreview.src = input;
+  }
 }
