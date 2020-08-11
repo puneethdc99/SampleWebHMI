4a9898d1b0b7fb68d205260114da815b486067d6
puneethdc99 <58505120+puneethdc99@users.noreply.github.com>
2020-08-11 12:30:38 +0530

    LogOff

--- click.js
+++ click.js
@@ -2,12 +2,6 @@ function LoadSecondImage() {
   document.getElementById("fullImage").src = "./images/Display2.png";
 }
 
-function LaunchOSk() {
-  var oShell = new ActiveXObject("Shell.Application");
-  var commandtoRun = "C:\\Windows\\System32\\osk.exe";
-  oShell.ShellExecute(commandtoRun, "", "", "open", "1");
-}
-
 function LoadFirstImage() {
   document.getElementById("fullImage").src = "./images/Display1.png";
 }
@@ -24,8 +18,8 @@ function saveImage() {
   var imagedata = document.getElementById("fullImage").src;
   if (imagedata.includes("shapes.svg")) {
     document
-      .getElementById("printbtn")
-      .setAttribute("href", "./images/Display4.png");
+    .getElementById("printbtn")
+    .setAttribute("href", "./images/Display4.png");
   } else {
     document.getElementById("printbtn").setAttribute("href", imagedata);
   }
